43c19c16985d8843b670a705d366a905d32aad1e
Manik-Jtg <manik.anand@joshtechnologygroup.com>
2021-10-09 11:18:56 +0530

    JIRA IMPLEMENTATION

--- PokerPlanner/apps/pokerboard/admin.py
+++ PokerPlanner/apps/pokerboard/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from apps.pokerboard.models import Pokerboard
+from apps.pokerboard.models import Pokerboard, Ticket
 
 admin.site.register(Pokerboard)
+admin.site.register(Ticket)
--- PokerPlanner/apps/user/urls.py
+++ PokerPlanner/apps/user/urls.py
@@ -1,4 +1,5 @@
-from django.urls.conf import path
+from django import urls
+from django.urls.conf import include, path
 from rest_framework.routers import DefaultRouter
 from apps.user.views import Login, Logout, UserViewSet
 
@@ -6,6 +7,7 @@ router = DefaultRouter()
 router.register('', UserViewSet)
 
 urlpatterns = [
+    path('/',include(router.urls)),
     path('login/', Login.as_view(), name="login"),
     path('logout/', Logout.as_view(), name="logout")
 ] + router.urls
